55ac30a807efa553b0d2c0374e9021c5a420cd44
saumer <jeremy.brish@gmail.com>
2016-04-29 14:06:52 -0400

    Beginings of tests

---
+++ src/main/java/com/seleniumTesting/selenium/HomePage.java
@@ -0,0 +1,10 @@
+package com.seleniumTesting.selenium;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by Jeremy on 4/29/2016.
+ */
+public class HomePage {
+    public static final By SearchButton = By.className("button flat");
+}
---
+++ src/test/java/com/seleniumTesting/selenium/HomePageTest.java
@@ -0,0 +1,21 @@
+package com.seleniumTesting.selenium;
+
+/**
+ * Created by Jeremy on 4/29/2016.
+ */
+//import io.ddavison.conductor.Browser;
+//import io.ddavison.conductor.Config;
+//import io.ddavison.conductor.Locomotive;
+import org.junit.Test;
+
+//@Config(
+//    browser = Browser.FIREFOX,
+//    url = "www.jstor.org"
+//
+//)
+public class HomePageTest {
+    @Test
+    public void testSeachButtonExists(){
+//        validatePresent(HomePage.SearchButton);
+    }
+}
