4f362f05107dd551a56bbf38c751928d2f919968
saumer <jeremy.brish@gmail.com>
2016-04-29 22:06:26 -0400

    page object for home page is a little better.

--- src/test/java/com/seleniumTesting/selenium/HomePageTest.java
+++ src/test/java/com/seleniumTesting/selenium/HomePageTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import com.seleniumTesting.selenium.Driver;
@@ -24,13 +25,23 @@ public class HomePageTest {
         browser.get(HomePage.url);
         System.out.println(HomePage.searchButton);
         try{
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e)
         {
 
         }
-        Boolean isPresent = browser.findElements(By.id(HomePage.searchBox)).size() > 0;
+        Boolean isPresent = browser.findElements(HomePage.searchBox).size() > 0;
         System.out.println(isPresent);
         assert(isPresent);
+
+        isPresent = browser.findElements(HomePage.searchButton).size() > 0;
+        System.out.println(isPresent);
+        assert(isPresent);
+
+        browser.findElement(HomePage.searchBox).sendKeys("test text here");
+        browser.findElement(HomePage.searchButton).click();
+        ExpectedConditions.urlContains("doBasicSearch?Query=");
+
+
     }
 }
